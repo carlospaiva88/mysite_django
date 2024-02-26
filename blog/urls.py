6618ababc38812df67733becd5c3ba0b7203c5f1
Cjr88 <carloscjr70@gmail.com>
2024-02-26 10:48:58 -0300

    templates are ready to be used

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+
+from blog import views
 from .views.post_view import PostView
 
 urlpatterns = [
-    path('', PostView.as_view(), name='home')
+    path('', PostView.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+
 ]
