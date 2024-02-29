f9ee0080e3e14c2a6cf036927c4347644a658b11
Cjr88 <carloscjr70@gmail.com>
2024-02-28 21:52:00 -0300

    corrigindo falha das urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views.post_view import PostView
+from .views.post_view import PostDetail, PostView
 
 urlpatterns = [
-    path('', PostView.as_view(), name='home')
+    path('', PostView.as_view(), name='home'),
+    path('<slug:slug>/', PostDetail.as_view(), name='post_detail')
 ]
